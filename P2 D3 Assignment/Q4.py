55f0bb5b37548c914e5a9cb6fb444e0f65b90cdd
Sanket Hiremath <73591040+Sanket01Hiremath@users.noreply.github.com>
2023-08-06 10:10:05 +0530

    P2 D3 Assignment

---
+++ P2 D3 Assignment/Q4.py
@@ -0,0 +1,10 @@
+str1 = "PyNaTive"
+s1="";
+s2="";
+for char in str1:
+    if char.islower():
+        s1+=char
+    else:
+        s2+=char
+
+print(s1+s2)
---
+++ P2 D3 Assignment/Q5.py
@@ -0,0 +1,8 @@
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+list3=[]
+
+for i,j in zip(list1,list2):
+    list3.append(i+j);
+
+print(list3)
